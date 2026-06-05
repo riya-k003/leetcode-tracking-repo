c666bc69413a52a9d34b28f47101e088f737a3ff
Riya <161316727+riya-k003@users.noreply.github.com>
2026-06-05 18:11:25 +0530

    Time: 2 ms (71.27%), Space: 38.3 MB (81.74%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-
-struct Node{
-    int next;
-    int left;
-    char task;
-};
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<pair<int,char>>freq;
-        queue<Node>cooldown;
-        int t =0;
-        unordered_map<char , int>mp;
-        for(auto & ch : tasks){
+        int k = tasks.size();
+        unordered_map<char , int> mp;
+        priority_queue<pair<int , char>>pq;
+        int countMaxFreq = 0;
+        for (auto ch : tasks) {
             mp[ch]++;
         }
-        for(auto & m: mp){
-            freq.push({m.second , m.first});
+        for(auto ch : mp){
+            pq.push({ch.second, ch.first});
         }
-        while(!cooldown.empty() || !freq.empty()){
-             while(!cooldown.empty() && cooldown.front().next == t){
-                    freq.push({cooldown.front().left , cooldown.front().task });
-                    cooldown.pop();
-                }
-                
-        if(!freq.empty()){
-                char c = freq.top().second;
-                int f = freq.top().first - 1;
-                freq.pop();
-                if(f!=0){
-                    int next = t+n+1;
-                    cooldown.push({next ,f , c});
-                }
-                t++;
+        int maxfreq = pq.top().first;
+        while(!pq.empty()){
+            if(pq.top().first == maxfreq){
+        countMaxFreq++;
             }
-          else{
-                t = cooldown.front().next;
-                }
+        pq.pop();
         }
-        return t;
+        // for (auto ch : mp) {
+        //     maxfreq = max(ch.second, maxfreq);
+        // }
+        
+        // for (auto p : mp) {
+        //     if (p.second == maxfreq) {
+        //         countMaxFreq++;
+        //     }
+        // }
+
+        int p = ((maxfreq - 1) * (n + 1) + countMaxFreq);
+        int ans = max(p, k);
+        return ans;
     }
 };
