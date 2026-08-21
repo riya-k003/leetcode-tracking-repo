b3a24899e5add37338fc2d53f2e7832232a4d189
Riya <161316727+riya-k003@users.noreply.github.com>
2026-08-21 12:13:39 +0530

    Time: 0 ms (100%), Space: 14.5 MB (15.86%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -5,27 +5,22 @@ public:
         int i =0 , j= tokens.size()-1;
         int score =0;
         int max_score =0;
-        if(tokens.size() == 0)return 0;
-        if(tokens[0] > power && score==0)return 0;
         while(i<=j){
             if(tokens[i]<=power){
                 score++;
                 power -= tokens[i];
                 i++;
+                 max_score = max(score , max_score);
             }
-            else if(tokens[j] <= power){
-                score++;
-                power-=tokens[j];
+            else if( score >0){
+                score--;
+                power+=tokens[j];
                 j--;
             }
-            else if( score >=1){
-                int max_token = max(tokens[i] , tokens[j]);
-                score--;
-                power+=max_token;
-                if(max_token == tokens[i])i++;
-                else j--;
-            }
-            max_score = max(score , max_score);
+            else{
+                break;
+                }
+           
         }
         return max_score;
     }
