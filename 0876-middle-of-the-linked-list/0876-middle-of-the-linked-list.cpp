dd572d1d9086ff1dfe65eeaca4facaa85002d558
Riya <161316727+riya-k003@users.noreply.github.com>
2026-08-26 19:24:49 +0530

    Time: 0 ms (100%), Space: 9.9 MB (90.99%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -13,10 +13,11 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        while(fast != NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-    return slow;
+        return slow;
+        
     }
 };
