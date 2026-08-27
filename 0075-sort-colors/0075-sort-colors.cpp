9a3fca4c22a4cf8c372590a5fac98bba41cd327a
Riya <161316727+riya-k003@users.noreply.github.com>
2026-08-28 00:31:57 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.62%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int i =0 , j=0 , k= n-1;
+            int i=0 , j= 0 , k=n-1;
             while(j<=k){
                 if(nums[j] == 0){
                     swap(nums[i] , nums[j]);
@@ -10,10 +10,10 @@ public:
                     j++;
                 }
                 else if(nums[j] == 1)j++;
-                else if(nums[j] == 2){
+                else if(nums[j]==2){
                     swap(nums[j] , nums[k]);
                     k--;
                 }
-            }        
+       }
     }
 };
