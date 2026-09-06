cca5ed546554521e91019a682ff1e60f76af3017
Riya <161316727+riya-k003@users.noreply.github.com>
2026-09-06 22:54:34 +0530

    Time: 32 ms (85.99%), Space: 85 MB (34.51%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,41 +1,19 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int n = fruits.size();
-        unordered_map<int , int>mp;
-        if(n<2)return 1;
-        if (n==2)return 2;
-        int l =0 , r =0;
+        int l=0;
         int maxi =0;
-        int lastTypeStart =0;
-        while(r<n){
-            if(mp.size() == 2 && mp.find(fruits[r]) == mp.end()){
-                int cnt =0;
-                for(auto el : mp){
-                    cnt += el.second;
-                }
-                maxi = max(cnt , maxi);
-                while(l<lastTypeStart){
-                    mp[fruits[l]]--;
-                    if(mp[fruits[l]] == 0) mp.erase(fruits[l]);
-                    l++;
-                }
-                mp[fruits[r]]++;
-            }else if(mp.size() <=2 && mp.find(fruits[r]) != mp.end()){
-                mp[fruits[r]]++;
-                     int cnt =0;
-                for(auto el : mp){
-                    cnt += el.second;
-                }
-                maxi = max(cnt , maxi);
-            }
-            else{
+        unordered_map<int , int>mp;
+        for(int r =0 ; r<fruits.size() ; r++){
             mp[fruits[r]]++;
+            while(mp.size() > 2){
+                mp[fruits[l]]--;
+                if(mp[fruits[l]] == 0){
+                    mp.erase(fruits[l]);
+                }
+                l++;
             }
-            if(r-1>= 0 && fruits[r-1] != fruits[r]){
-                lastTypeStart = r;
-            }
-            r++;
+            maxi = max(r-l+1 ,maxi);
         }
         return maxi;
     }
